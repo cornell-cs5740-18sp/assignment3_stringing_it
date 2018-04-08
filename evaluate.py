8fc4d1c7f42f37be64aa61569f1d4d4244978b25
noopurP <parikh.noopur18@gmail.com>
2018-04-08 15:50:48 -0400

    updated evaluate.py by TA

--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import f1_score, confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 from argparse import ArgumentParser
 
 
@@ -30,6 +30,7 @@ dev.columns  = ["actual"]
 
 data = dev.join(pred)
 
+print("Accuracy: ", accuracy_score(data.actual, data.predicted))
 
 if args.show_confusion:
 
@@ -41,11 +42,3 @@ if args.show_confusion:
 
     if confusion.empty: print("None!")
     else: print(confusion)
-
-else:
-
-    print("Mean F1 Score:", f1_score(
-        data.actual,
-        data.predicted,
-        average = "weighted"
-    ))
